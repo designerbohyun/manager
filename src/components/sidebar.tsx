a273b96b71ca85359cc024200853b66c438b5c1d
김보현 <kimbohyunm12@gmail.com>
2024-02-14 21:52:21 +0900

    update sidebar components

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Box, Link } from "@chakra-ui/layout";
 import { TeritaryBtn } from "./buttons/button";
 import { MdCalendarToday, MdOutlineInventory2 } from 'react-icons/md';
-import { Image } from '@chakra-ui/react'
+import { Img } from '@chakra-ui/react';
 import AccordionBtn from "./accordion";
 
 interface SidebarProps {
@@ -12,7 +12,7 @@ interface SidebarProps {
 export const SideBar = () => {
     return (
         <Box minW="14.375rem" maxW="14.375rem" h="100%" py={4} px={3} borderRight="1px" borderRightColor="gray.200" boxShadow="md" backgroundColor="white" display="flex" flexDirection="column" gap={2}>
-            <Link><Image src="./assets/logo.png" alt="Manager Logo"></Image></Link>
+            <Link w="70%" px={4} my={2}><Img src="https://i.imgur.com/DpCyo8z.png" alt="Manager Logo"></Img></Link>
             <TeritaryBtn w="100%" leftIcon={<MdCalendarToday />} buttonText="예약 현황"></TeritaryBtn>
             <AccordionBtn></AccordionBtn>
             <TeritaryBtn w="100%" leftIcon={<MdOutlineInventory2 />} buttonText="상품 관리"></TeritaryBtn>
